7fbdfd97b8eb509d2691b8d2b00567dbca1a49b4
Carakaton <carakaton@gmail.com>
2023-06-19 00:35:51 +0300

    Docker compose support added

--- back-fastapi/src/main.py
+++ back-fastapi/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+import time
 
 from books.router import router as books_router
 from database import database
@@ -20,4 +21,4 @@ async def shutdown():
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('main:app', host='localhost', port=8000, reload=True)
