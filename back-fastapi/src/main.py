daafd3cc4e748b5a9c5351adb62d5b613072c70c
Carakaton <carakaton@gmail.com>
2023-06-20 22:46:29 +0300

    CORS policy is updated

--- back-fastapi/src/main.py
+++ back-fastapi/src/main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-import time
 
 from books.router import router as books_router
 from database import database
 
 
 app = FastAPI(title='ReadShare')
+
 app.include_router(books_router)
+
+origins = [
+    'http://localhost:8080',
+    'http://localhost:8000',
+    'http://localhost:5173',
+    'http://localhost'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH', 'PUT'],
+    allow_headers=['Content-Type', 'Set-Cookie', 'Authorization', 
+                   'Access-Contol-Allow-Headers', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods']
+)
 
 
 @app.on_event("startup")
