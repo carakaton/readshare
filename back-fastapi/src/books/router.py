dac6b40734789fc9e8d24f14f89d188d21b983a0
Carakaton <carakaton@gmail.com>
2023-06-25 17:23:35 +0300

    Global docker-compose file is deleted

--- back-fastapi/src/books/router.py
+++ back-fastapi/src/books/router.py
@@ -15,6 +15,102 @@ router = APIRouter(
     tags=['Books']
 )
 
+BOOKS = {
+  "count": 18,
+  "books": [
+    {
+      "livelib_id": 1001257165,
+      "title": "Дракон",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1000383103,
+      "title": "Дракон. Повести. Пьесы. Сценарии (сборник)",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1000493344,
+      "title": "Тень. Дракон. Обыкновенное чудо (сборник)",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1000455964,
+      "title": "Обыкновенное чудо. Голый король. Тень. Дракон. Снежная королева. Два клена. Золушка (сборник)",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1000529123,
+      "title": "Дракон. Пьесы (сборник)",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1000234105,
+      "title": "Голый король. Снежная королева. Тень. Дракон. Два клена. Обыкновенное чудо. Золушка. Дон-Кихот (сборник)",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1001456315,
+      "title": "Лекция «Eвгений Шварц и тайна дракона»",
+      "author": "Дмитрий Быков"
+    },
+    {
+      "livelib_id": 1001833566,
+      "title": "Лекция «Евгений Шварц и тайна дракона. Часть 3-я»",
+      "author": "Дмитрий Быков"
+    },
+    {
+      "livelib_id": 1000580634,
+      "title": "Обыкновенное чудо. Голый король. Тень. Дракон",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1000451207,
+      "title": "Пьесы: Клад. Красная шапочка. Снежная королева. Тень. Дракон. Два клена. Обыкновенное чудо. Повесть о молодых супругах. Золушка. Дон Кихот (сборник)",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1002105656,
+      "title": "Обыкновенное чудо. Дракон (сборник)",
+      "author": "Шварц Евгений Львович"
+    },
+    {
+      "livelib_id": 1000222533,
+      "title": "Голый король. Снежная королева. Тень. Дракон. Пьесы 1934 - 1943 (сборник)",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1001438270,
+      "title": "Лекция «Евгений Шварц и тайна дракона». лекция 2.",
+      "author": "Дмитрий Быков"
+    },
+    {
+      "livelib_id": 1007394436,
+      "title": "Дракон",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1002889767,
+      "title": "Дракон (сборник)",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1001831976,
+      "title": "Лекция «Евгений Шварц и тайна дракона. Часть 2-я»",
+      "author": "Дмитрий Быков"
+    },
+    {
+      "livelib_id": 1003035665,
+      "title": "Дракон над Москвой. Сборник рассказов",
+      "author": "Евгений Шварц"
+    },
+    {
+      "livelib_id": 1005712495,
+      "title": "Снежная королева. Дракон. Пьесы-сказки.",
+      "author": "Евгений Шварц"
+    }
+  ]
+}
+
 
 @router.get('/all')
 async def get_all_books():
@@ -41,7 +137,7 @@ async def find_books(search_query: str, offset: int = 0) -> FoundBooks:
     """ Поиск книг """
 
     found_books = await to_thread(get_books_basic_info, search_query, offset)
-    return found_books
+    return BOOKS
 
 
 @router.get('/{id}')
@@ -62,7 +158,16 @@ async def get_book(id: int) -> Optional[FullBook]:
 async def add_book(livelib_id: int) -> dict:
     """ Добавление книги """
 
-    book = await to_thread(get_full_book, livelib_id)
+    # book = await to_thread(get_full_book, livelib_id)
+
+    book, = filter(lambda book: book if book['livelib_id'] == livelib_id else None, BOOKS['books'])
+    book = FullBook(
+        id=book['livelib_id'],
+        title = book['title'],
+        author = book['author'],
+        cover_url = None
+    )
+
     query = insert(Book).values(**book.dict())
     try:
         await database.execute(query)
