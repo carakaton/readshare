b53bade4785fe1b4451d45e792d1c147007f59c0
Carakaton <carakaton@gmail.com>
2023-06-30 02:05:43 +0300

    Mom i dont want to die

--- back-fastapi/src/collection/router.py
+++ back-fastapi/src/collection/router.py
@@ -19,71 +19,19 @@ router = APIRouter(
 async def get_books(user: User = Depends(get_current_user)) -> FullBooks:
     """ Книги текущего пользователя """
     
-    query = select(BookUser).join(Book).where(BookUser.user_id==user.id)
-    books= await database.fetch_all(query)
-    
-    full_books = [FullBook(
-        id = book.id,
-        title = book.title,
-        author = book.author,
-        cover_url = book.cover_url
-    ) for book in books or []] 
+    query = select(BookUser).where(BookUser.user_id==user.id)
+    books_user = await database.fetch_all(query)
 
-    return FullBooks(
-        count = len(full_books),
-        books = full_books
-    )
-
-
-@router.post('/')
-async def add_book(book_id: int, user: User = Depends(get_current_user)):
-    """ Добавить книгу текущему пользователю """
-
-    query = insert(BookUser).values(book_id=book_id, user_id=user.id)
-    await database.execute(query)
-    
-    return Response(status_code=status.HTTP_201_CREATED)
-
-
-
-@router.delete('/')
-async def remove_book(book_id: int, user: User = Depends(get_current_user)):
-    """ Все книги текущего пользователя """
-    
-    query = delete(BookUser).where(book_id=book_id, user_id=user.id)
-    await database.execute(query)
-    
-    return Response(status_code=status.HTTP_200_OK)
-from fastapi import APIRouter, Depends, status, Response
-from sqlalchemy import insert, select, delete
-
-from database import database
-from users.models import User
-from users.core import get_current_user
-from books.models import Book
-from books.schemas import FullBooks, FullBook
-from .models import BookUser
-
-
-router = APIRouter(
-    prefix='/collection',
-    tags=['Collection']
-)
-
-
-@router.get('/')
-async def get_books(user: User = Depends(get_current_user)) -> FullBooks:
-    """ Книги текущего пользователя """
-    
-    query = select(BookUser).join(Book).where(BookUser.user_id==user.id)
-    books= await database.fetch_all(query)
-    
-    full_books = [FullBook(
-        id = book.id,
-        title = book.title,
-        author = book.author,
-        cover_url = book.cover_url
-    ) for book in books or []] 
+    full_books = []
+    for book_user in books_user:
+        query = select(Book).where(Book.id==book_user.book_id)
+        book = await database.fetch_one(query)
+        full_books.append(FullBook(
+            id = book.id,
+            title = book.title,
+            author = book.author,
+            cover_url = book.cover_url
+        ))
 
     return FullBooks(
         count = len(full_books),
